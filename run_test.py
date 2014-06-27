66f73abe260c6c9de368dfa8bd40a2399c0b32c1
tomthoros <tom.laigna@gmail.com>
2014-06-27 23:15:59 +0300

    Update run_test.py
    First argument is number of random numbers.
    10^6 by default.

--- run_test.py
+++ run_test.py
@@ -1,11 +1,9 @@
-import os
+import os, sys, multiprocessing, StringIO
 from subprocess import Popen, PIPE
-import StringIO
-import multiprocessing
 
 def runTest(threads, numbers):
   process = Popen("ThreadedMonteCarloPi.exe", stdout=PIPE, stdin=PIPE)
-  process.stdin.write(str(threads) + '\n' + str(numbers))
+  process.stdin.write(str(threads) + '\n' + str(numbers) + '\n')
   (output, err) = process.communicate()
   exit_code = process.wait()
 #  print (output)
@@ -14,10 +12,16 @@ def runTest(threads, numbers):
       exec_time = line.split(" ")[2]
       return "exec time: " + exec_time + " milliseconds"
 
-N = 1000000
+if len(sys.argv) > 1:
+  N = int(sys.argv[1])
+else:
+  N = 1000000
+
 total_cores = multiprocessing.cpu_count()
 best_time = 0
 best_threads = 0
 
-for cores in range (1, 8 * (total_cores + 1)):
-  print runTest(cores, N / cores) + " with " + str(cores) + " cores and a total of " + str(N) + " random numbers"
+print "Running test with total of " + str(N) + " random numbers"
+
+for cores in range (1, 4 * (total_cores + 1)):
+  print runTest(cores, N / cores) + " with " + str(cores) + " cores"
